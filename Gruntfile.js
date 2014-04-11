6dd4a574a0c935c029fec62e039dc45d43c88af3
Naren Ranjit <narendran.ranjit@gmail.com>
2014-04-11 06:52:17 -0700

    init

---
+++ Gruntfile.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function(connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function(grunt) {
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    var ejs = require('ejsify');
+
+    grunt.initConfig({
+        watch: {
+            compass: {
+                files: ['src/styles/**/*.scss'],
+                tasks: ['compass']
+            },
+            js: {
+                files: [ 'src/scripts/**/*.js'],
+                tasks: 'browserify2'
+            },
+            index: {
+                files: [ 'src/index.html'],
+                tasks: 'copy:index'
+            },
+            options: {
+                nospawn: true,
+                livereload: {
+                    port: LIVERELOAD_PORT
+                }
+            }
+        },
+        copy: {
+            index: {
+                src: 'src/index.html',
+                dest: 'public/index.html'
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                directory: './public'
+
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, './public')
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        browserify2: {
+            dev: {
+                entry: './src/scripts/app.js',
+                compile: './public/scripts/app.js',
+                debug: true,
+                options: {
+                    expose: {
+                        files: [
+                            {
+                                cwd: 'src/scripts/',
+                                src: ['**/*.ejs'],
+                                dest: 'templates/'
+                            },
+                            {
+                                cwd: 'src/scripts/views/',
+                                src: ['**/*.js'],
+                                dest: 'views/'
+                            },
+                            {
+                                cwd: 'src/scripts/models/',
+                                src: ['**/*.js'],
+                                dest: 'models/'
+                            },
+                            {
+                                cwd: 'src/scripts/collections/',
+                                src: ['**/*.js'],
+                                dest: 'collections/'
+                            }
+                        ]
+                    }
+                },
+                beforeHook: function (bundle) {
+                    return bundle.transform(ejs);
+                }
+            }
+        },
+
+
+        compass: {
+            dev: {
+                options: {
+                    sassDir: 'src/styles',
+                    cssDir: 'public/styles',
+                    imagesDir: 'public/styles/assets/images',
+                    javascriptsDir: 'public/scripts',
+                    fontsDir: 'public/styles/assets/fonts',
+                    environment: 'dev',
+                    outputStyle: 'expanded',
+                    relativeAssets: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('server', ['connect:livereload', 'open', 'watch']);
+    grunt.registerTask('init', ['compass', 'browserify2']);
+
+
+    grunt.registerTask('default', ['watch']);
+};
---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+;(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
+'use strict';
+
+$(function() {
+	console.log('Hello there!');
+});
+},{}]},{},[1])
+//@ sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2VuZXJhdGVkLmpzIiwic291cmNlcyI6WyIvVXNlcnMvbmFyZW5yYW5qaXQvRlByanMvcm91dGUtb3B0aW1pemVyL3NyYy9zY3JpcHRzL2FwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXNDb250ZW50IjpbIid1c2Ugc3RyaWN0JztcblxuJChmdW5jdGlvbigpIHtcblx0Y29uc29sZS5sb2coJ0hlbGxvIHRoZXJlIScpO1xufSk7Il19
+;
