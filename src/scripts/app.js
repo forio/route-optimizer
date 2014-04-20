9aa343490ce1dff3c7e455a614419ad227539241
Naren Ranjit <narendran.ranjit@gmail.com>
2014-04-19 18:13:00 -0700

    basic donut

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,4 +2,34 @@
 
 $(function() {
 	console.log('Hello there!');
+
+    var formatter = d3.format('%');
+
+    Contour.export('donutTextOneValue', function (data, layer, options) {
+        var visibleIndex = data[0].data[0].y < data[0].data[1].y ? 1 : 0;
+        var textEl = layer.append('text')
+            .attr('class', 'center-text')
+            .attr('x', 29) //29
+            .attr('y', 58) //58
+            .text(formatter(data[0].data[visibleIndex].y));
+    });
+
+    var data = [{ x: 'Case A', y: 0.82}, { x: 'Case B', y: 0.18 }];
+
+    // new Contour({
+    //         el: '.pie-gauge',
+    //         pie: {
+    //             piePadding: 0,
+    //             innerRadius: 40,
+    //             outerRadius: 50
+    //         },
+    //         tooltip: {
+    //             formatter: function(d) {
+    //                 return d.data.x + ': ' + formatter(d.data.y);
+    //             }
+    //         }
+    //     })
+    //     .pie(data)
+    //     .donutTextOneValue(data)
+    //     .render();
 });
