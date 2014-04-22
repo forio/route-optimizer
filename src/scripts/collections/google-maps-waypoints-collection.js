35323f79c5fd8245c2164d37e99d723e666dd68e
Naren Ranjit <narendran.ranjit@gmail.com>
2014-04-21 22:12:25 -0700

    get directions

--- src/scripts/collections/google-maps-waypoints-collection.js
+++ src/scripts/collections/google-maps-waypoints-collection.js
@@ -8,11 +8,50 @@ module.exports = BaseCollection.extend({
         return 34;
     },
 
+    getDistanceFromRoute: function(gDirectionsRoutes) {
+        // return _.reduce(gDirectionsResult, function(routeMemo, gDirectionsRoute){
+        //     return routeMemo + _.reduce(gDirectionsRoute,  function (legMemo, gDirectionLegs){
+        //         return legMemo +
+        //     });
+        // });
+
+        var totalDistance = 0;
+        _(gDirectionsRoutes).each(function(gDirectionsRoute){
+            _(gDirectionsRoute).each(function (gDirectionLegs){
+                _(gDirectionLegs.legs).each(function (leg) {
+                    totalDistance += leg.distance.value;
+                });
+            });
+        });
+        return totalDistance;
+    },
+
     getDirections: function(startIndex, destIndex) {
         var startModel = this.at(startIndex);
-        var destModel = this.get(destIndex);
+        var destModel = this.at(destIndex);
 
-        // var request = google.maps.DirectionsRequest();
+        var $def = $.Deferred();
 
+        var request = ({
+            origin: startModel.getLatLong(),
+            destination: destModel.getLatLong(),
+            travelMode: 'DRIVING'
+        });
+
+        var me = this;
+        var ds = new google.maps.DirectionsService();
+        ds.route(request, function(directionsResult, status) {
+            if (status === 'OK') {
+                var distance = me.getDistanceFromRoute(directionsResult);
+                console.log('distance between', startModel.get('name'), 'and', destModel.get('name'), 'is', distance);
+                me.distances[startIndex] = distance;
+                $def.resolve(directionsResult);
+            }
+            else {
+                $def.reject(status);
+            }
+        });
+
+        return $def;
     }
 });
