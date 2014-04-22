0fe8326a1c0fb1bc4ce3692f31eb448ffa1c0257
Naren Ranjit <narendran.ranjit@gmail.com>
2014-04-22 08:09:26 -0700

    waypoint selection handlers

--- src/scripts/views/map/waypoint-marker-view.js
+++ src/scripts/views/map/waypoint-marker-view.js
@@ -1,9 +1,22 @@
 var BaseView = require('views/waypoint-item-view');
 module.exports = BaseView.extend({
 
+
+    selectPoint: function() {
+        var isSelected = this.model.get('selected');
+        var method = (isSelected) ? 'addClass' : 'removeClass';
+        this.$el[method]('selected');
+
+        this.marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        this.marker.setIcon({
+            fillColor: 'white'
+        });
+    },
+
    render: function() {
         var myLatlng = this.model.getLatLong();
-        var marker = new google.maps.Marker({
+         this.marker = new google.maps.Marker({
               position: myLatlng,
               map: this.map,
               title: this.model.get('name')
