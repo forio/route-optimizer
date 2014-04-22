5aae3e6d7477714aa89962ade21453378d6d98bb
Naren Ranjit <narendran.ranjit@gmail.com>
2014-04-22 11:11:05 -0700

    more epicenter services

--- src/scripts/services/epicenter-operations-service.js
+++ src/scripts/services/epicenter-operations-service.js
@@ -1,19 +1,29 @@
-var RunsService = require('services/epicenter-runs-service');
+var runsService = require('services/epicenter-runs-service');
+var urlService = require('services/epicenter-url-service');
 
 module.exports = function() {
 
     var runid = "";
+    var apiURL = urlService.getApiURL('operation');
 
     return {
 
         do: function (operation, data) {
             var $def = $.Deferred();
             var postData = {name: operation, arguments:[data]};
-            RunsService.getRunID().then(function(runid) {
-                $.post('/api/operations/' + runid, postData, function (response) {
+
+            var doPOST = function (runid) {
+                $.post(apiURL + '/' + runid, postData, function (response) {
                     $def.resolve(response);
-                }) ;
-            });
+                });
+            };
+
+            runsService.getRunID()
+                .then(doPOST)
+                .fail(function (){
+                    //Run must've gone out of memory
+                    runsService.getRunID(true).then(doPOST);
+                });
             return $def;
         }
     };
--- src/scripts/services/epicenter-runs-service.js
+++ src/scripts/services/epicenter-runs-service.js
@@ -1,10 +1,24 @@
+var urlService = require('services/epicenter-url-service');
+
 module.exports = function() {
     var runid;
+    var runURL = urlService.getApiURL('run');
 
     return {
+        getRunID: function (forceGet) {
+            var $def = $.Deferred();
 
-        getRunID: function () {
+            if (!runid || forceGet) {
+                $.post(runURL, function (run){
+                    runid = run.id;
+                    $def.resolve(run.id);
+                });
+            }
+            else {
+                $def.resolve(runid);
+            }
 
+            return $def;
         },
 
         create: function () {
