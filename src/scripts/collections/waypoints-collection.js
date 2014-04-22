fc0b398004a8b7db4db11a4b398b8db331fcd9ab
Naren Ranjit <narendran.ranjit@gmail.com>
2014-04-21 21:21:57 -0700

    markers on the map

--- src/scripts/collections/waypoints-collection.js
+++ src/scripts/collections/waypoints-collection.js
@@ -2,6 +2,7 @@ var waypointModel = require('models/waypoint-model');
 
 module.exports = Backbone.Collection.extend({
     model: waypointModel,
+    url : '/data/books.json',
 
     getMetric: function(metric) {
         return 34;
--- src/scripts/views/map/map-view.js
+++ src/scripts/views/map/map-view.js
@@ -3,12 +3,6 @@ var MarkerView = require('views/waypoint-marker-view');
 var API_KEY = "AIzaSyCTW23vXahKOwTwLIAKftgLHU9gHF0etXU";
 
 module.exports = Backbone.View.extend({
-
-   initialize: function() {
-
-
-   },
-
    render: function() {
         this.renderSelf();
         this.renderWaypoints();
@@ -26,18 +20,17 @@ module.exports = Backbone.View.extend({
             center: new google.maps.LatLng(37.772207, -122.450550)
 
           };
-          var map = new google.maps.Map(this.el,mapOptions);
-
+        this.map = new google.maps.Map(this.el,mapOptions);
         return this;
    },
 
    renderWaypoints: function() {
         this.collection.each(function(mdl) {
             var mv = new MarkerView({
-                model: mdl
+                model: mdl,
+                map: this.map
             });
-
-            this.$el.append(mv.render().$el);
+            mv.render();
         }, this);
         return this;
    }
