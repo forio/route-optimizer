070c2f277f10484e7ea336161724b2310f15f1d3
Naren Ranjit <narendran.ranjit@gmail.com>
2014-04-24 15:42:05 -0700

    donut chart updates

--- src/scripts/views/donut-chart-view.js
+++ src/scripts/views/donut-chart-view.js
@@ -2,7 +2,7 @@ var BaseView = require('views/base-view');
 
 Contour.export('donutTextOneValue', function (data, layer, options) {
     var val = data[0].data[0].y;
-    var text = (val !== 0) ? (val * 100) + '%' : '';
+    var text = (val !== 0) ? parseInt( val * 100, 10) + '%' : '';
     var textEl = layer.append('text')
                     .attr('class', 'center-text')
                     .attr('x', 12)
@@ -14,10 +14,29 @@ Contour.export('donutTextOneValue', function (data, layer, options) {
 module.exports = BaseView.extend({
     template: require('templates/donut-chart'),
 
+    initialize: function () {
+           this.original = this.model.get('original');
+           this.optimized = this.model.get('optimized');
+
+           this.original.on('routesRecalculated', this.render, this);
+           this.optimized.on('routesRecalculated', this.render, this);
+
+           BaseView.prototype.initialize.apply(this, arguments);
+    },
+
+    getData: function () {
+        var originalVal = this.dataSource(this.original);
+        var optimizedVal = this.dataSource(this.optimized);
+
+        var difference = optimizedVal / originalVal;
+        return difference;
+     },
+
     render: function () {
-        if (this.value && this.value !== Infinity) {
+        var value = this.getData();
+        if (value && value !== Infinity) {
             this.renderSelf();
-            this.renderChart();
+            this.renderChart(value);
         }
         else {
             this.$el.empty();
@@ -26,8 +45,8 @@ module.exports = BaseView.extend({
     renderSelf: function () {
         this.$el.html(this.template());
     },
-    renderChart: function() {
-        var data = [{ x: 'Gain', y: this.value}, { x: 'Rest', y: 1 - this.value }];
+    renderChart: function(value) {
+        var data = [{ x: 'Gain', y: 1- value}, { x: 'Rest', y: value }];
         if (!this.graph) {
             this.graph =  new Contour({
                 el: this.$('.graph').get(0),
