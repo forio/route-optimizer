a27ed701a7b3cc7326095dd82356a2c7b4a56ccb
Naren Ranjit <narendran.ranjit@gmail.com>
2014-05-02 07:20:19 -0700

    fleshed out scenario view

--- src/scripts/views/scenarios/scenarios-view.js
+++ src/scripts/views/scenarios/scenarios-view.js
@@ -1,8 +1,23 @@
 
 module.exports = Backbone.View.extend({
     template: require('templates/scenarios'),
+
+    scenarios: {
+        'art-gallery': 'San Francisco Art Gallery',
+        'book-crawl': 'San Francisco Book Crawl',
+        'sf-sightseeing': 'San Francisco Sight-Seeing'
+    },
+
+    events: {
+        'change select': 'handleScenarioSelect'
+    },
+
+    handleScenarioSelect: function (evt) {
+        var url = $(evt.target).val();
+    },
+
     render: function() {
-        this.$el.html(this.template());
+        this.$el.html(this.template({scenarios: this.scenarios}));
         return this;
     }
 });
